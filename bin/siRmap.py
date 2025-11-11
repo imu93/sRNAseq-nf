11ab179f737159784a80ffab625666659a79eb18
Isaac Martinez <isaacmtzu1293@gmail.com>
2025-11-11 15:07:24 -0500

    siRmap

--- bin/siRmap.py
+++ bin/siRmap.py
@@ -884,15 +884,19 @@ def _clear_secondary_and_supplementary(aln: pysam.AlignedSegment):
     # Make sure it's marked as mapped
     aln.flag &= ~0x4
 
+
+ 
+ 
 def _expand_write(read: pysam.AlignedSegment, out_bam: pysam.AlignmentFile, rc: int, zt_value: str | None = None):
     """
-    Write 'rc' copies of aln to out_bam after sanitizing
-    Each copy has NH=1 and RC=1 and no multimapper/supplementary tags
+    Write 'rc' copies of aln to out_bam after sanitizing Each copy 
+    has NH=1 and RC=1 and no multimapper/supplementary tags
     """
-    _clear_secondary_and_supplementary(read)
-    # add ZT tag whcih can be used to track unique/multimapper status
-    # also I will use it to  track probabilities associated with UWM
+    # sanitize in-place (okay to mutate a temp object)
+    # add ZT tag whcih can be used to track unique/multimapper status 
+    # also I will use it to track probabilities associated with UWM 
     # when radom, the probability is 1.0
+    _clear_secondary_and_supplementary(read)
     if zt_value is not None:
         try:
             read.set_tag("ZT", zt_value, value_type='Z')
@@ -900,22 +904,24 @@ def _expand_write(read: pysam.AlignedSegment, out_bam: pysam.AlignmentFile, rc: 
             pass
     _sanitize_for_out_inplace(read)
 
-    # build a short token from QNAME
+    # Sabe read names
     orig_qname = read.query_name
-    # Use rea read info to build the token
-    raw = f"{orig_qname}|{read.reference_name}|{read.reference_start}|{read.reference_end}|{int(read.is_reverse)}"
-    token = hashlib.blake2b(raw.encode("utf-8"), digest_size=6).hexdigest()
-    # add the token to the original ID
-    # but try to keep the same length
-    extra = 1 + len(token)
-    if len(orig_qname) + extra > 254:
-        base = orig_qname[: 254 - extra]
-    else:
-        base = orig_qname
-    # fix ID
-    read.query_name = f"{base}_{token}"
-    # write the bam
-    out_bam.write(read)
+    total = int(rc)
+
+    for i in range(1, total + 1):
+        #  Use rea read info to build the token
+        raw = f"{orig_qname}|{i}|{total}|{read.reference_name}|{read.reference_start}|{read.reference_end}|{int(read.is_reverse)}"
+        token = hashlib.blake2b(raw.encode("utf-8"), digest_size=6).hexdigest()
+        # add the token to the original ID
+        # but try to keep the same length
+        extra = 1 + len(token)
+        base = orig_qname if len(orig_qname) + extra <= 254 else orig_qname[:254 - extra]
+        # QNAME único por copia
+        read.query_name = f"{base}_{token}"
+        # Write the copy
+        out_bam.write(read)
+
+ 
 
 # Group alignments by query name to process multimappers together
 def _group_alignments_by_qname(in_bam_path: str):
